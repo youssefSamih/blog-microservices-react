e1b9a4c1f34563d453110d567a7c5904ff2f99b8
Youssef Samih <youssef.samih@4d.com>
2021-02-01 03:40:20 +0100

    chore: update comment component with new list

---
+++ src/CommentList.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const CommentList = ({ postId }) => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(
+        `http://localhost:4001/posts/${postId}/comments`
+      );
+      setComments(res.data);
+    };
+    fetchData();
+  }, [postId]);
+
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+  return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;
--- src/PostList.js
+++ src/PostList.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 const PostList = () => {
   const [posts, setPosts] = useState([{}]);
@@ -10,15 +12,17 @@ const PostList = () => {
   useEffect(() => {
     fetchPosts();
   }, []);
-  const renderedPosts = Object.values(posts).map((post) => {
+  const renderedPosts = Object.values(posts).map((post, id) => {
     return (
       <div
         className="card"
         style={{ width: "30%", marginBottom: 20 }}
-        key={post.id}
+        key={`${post.id}-${id}`}
       >
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentList postId={post.id} />
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
