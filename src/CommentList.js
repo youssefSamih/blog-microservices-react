309340d59cd9da8f548f795768a33eaced1ba9e1
Youssef Samih <youssef.samih@4d.com>
2021-02-02 22:29:37 +0100

    feat: query event

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,20 +1,5 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await axios.get(
-        `http://localhost:4001/posts/${postId}/comments`
-      );
-      setComments(res.data);
-    };
-    fetchData();
-  }, [postId]);
-
-  const renderedComments = comments.map((comment) => {
+const CommentList = ({ comments }) => {
+  const renderedComments = comments?.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
   });
   return <ul>{renderedComments}</ul>;
